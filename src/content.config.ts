feebc43a06f4fe642971a79258f79503ae599d9c
foolproof-htb <fujisawa.htb@gmail.com>
2025-10-07 19:51:24 +0900

    記事のヘッダー画像がデフォルトで付くようにする

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import defaultHeroImage from './assets/default-hero-image.jpg';
 
 const post = defineCollection({
   // Load Markdown and MDX files in the `src/content/post/` directory.
   loader: glob({ base: './src/content/post', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string().default(''),
-      tags: z.array(z.string()),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
-    }),
+    z
+      .object({
+        title: z.string(),
+        description: z.string().default(''),
+        tags: z.array(z.string()),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: image().optional(),
+      })
+      .transform((data) => ({
+        ...data,
+        heroImage: data.heroImage ?? defaultHeroImage,
+      })),
 });
 
 export const collections = { post };
