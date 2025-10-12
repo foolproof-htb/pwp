01ff6837c34985980edc3c429fa120bf1697d730
foolproof-htb <fujisawa.htb@gmail.com>
2025-10-12 20:17:09 +0900

    サイト名を変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "foolproof_htb's website";
-export const SITE_DESCRIPTION = "Welcome to foolproof_htb's website!";
+export const SITE_TITLE = 'ぽろぽろポラライド';
+export const SITE_DESCRIPTION = 'Welcome to poroporo polaride.';
