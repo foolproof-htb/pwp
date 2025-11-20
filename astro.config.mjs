7574d2d7af962793ab6379c1c59df319e90e9f74
foolproof-htb <fujisawa.htb@gmail.com>
2025-11-20 18:32:35 +0900

    MDXを使用できるようにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,23 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+        },
+      ],
+    ],
+  },
 });
